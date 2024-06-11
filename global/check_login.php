58a83c5ffc0420e64379f2b9e794643b84c96bde
coquia-kdt <coquia-kdt@KDTW593>
2024-06-11 10:16:04 +0800

    check login sa mga active users lang

--- global/check_login.php
+++ global/check_login.php
@@ -34,18 +34,18 @@ if (!empty($_SESSION["IDKHI"])) {
 #region main function
 try {
     $empQ = "SELECT kd.`number` as `id`, kd.`surname`, kd.`firstname`, gl.`name` as `group` FROM `khi_details` as kd LEFT JOIN kdtphdb_new.`group_list` as gl 
-    ON gl.`id` = kd.`group_id` WHERE `number` = :userID";
+    ON gl.`id` = kd.`group_id` WHERE `number` = :userID AND `is_active`=1";
     $empStmt = $connpcs->prepare($empQ);
     $empStmt->execute([":userID" => "$userID"]);
     if ($empStmt->rowCount() > 0) {
         $empDetails = $empStmt->fetchObject();
         $checkAccess = allGroupAccess($userID);
-        if($checkAccess == true) {
+        if ($checkAccess == true) {
             $empDetails->type = 1;
         } else {
             $empDetails->type = 0;
         }
-  
+
         $result["isSuccess"] = true;
         $result["data"] = $empDetails;
     } else {
