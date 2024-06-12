a0e4f1ffdd739ee16c426458c3ae366dae4c74b2
coquia-kdt <coquia-kdt@KDTW593>
2024-06-12 15:45:12 +0800

    toasty sa login

--- login/js/individual.js
+++ login/js/individual.js
@@ -43,13 +43,17 @@ $(document).on("submit", "#loginForm", function (event) {
         // alert(`${res.message}`);
         $("#userid").val("");
       } else {
-        alert(`${res.message}`);
+        // alert(`${res.message}`);
+        showToast("error", `${res.message}`);
         $("#userid").val("");
       }
     })
     .catch((error) => {
       console.log(error);
     });
+});
+$(document).on("click", ".rmvToast", function () {
+  $(this).closest(".toasty").remove();
 });
 //#endregion
 
@@ -148,5 +152,51 @@ function Login() {
       },
     });
   });
+}
+//3 TYPES OF TOAST TO USE(success, error, warn)
+//EXAMPLE showToast("error", "error message eto")
+function showToast(type, str) {
+  let toast = document.createElement("div");
+  if (type === "success") {
+    toast.classList.add("toasty");
+    toast.classList.add("success");
+    toast.innerHTML = `
+    <i class='bx bx-check text-xl text-[var(--tertiary)]'></i>
+  <div class="flex flex-col py-3">
+    <h5 class="text-md font-semibold leading-2">Success</h5>
+    <p class="text-gray-600 text-sm">${str}</p>
+    <span><i class='rmvToast bx bx-x absolute top-[10px] right-[10px] text-[16px] cursor-pointer' ></i></span>
+  </div>
+    `;
+  }
+  if (type === "error") {
+    toast.classList.add("toasty");
+    toast.classList.add("error");
+    toast.innerHTML = `
+    <i class='bx bx-x text-xl text-[var(--red-color)]'></i>
+  <div class="flex flex-col py-3">
+    <h5 class="text-md font-semibold leading-2">Error</h5>
+    <p class="text-gray-600 text-sm">${str}</p>
+    <span><i class='rmvToast bx bx-x absolute top-[10px] right-[10px] text-[16px] cursor-pointer' ></i></span>
+  </div>
+    `;
+  }
+  if (type === "warn") {
+    toast.classList.add("toasty");
+    toast.classList.add("warn");
+    toast.innerHTML = `
+    <i class='bx bx-info-circle text-lg text-[#ffaa33]'></i>
+    <div class="flex flex-col py-3">
+      <h5 class="text-md font-semibold leading-2">Warning</h5>
+      <p class="text-gray-600 text-sm">${str}</p>
+      <span><i class='rmvToast bx bx-x absolute top-[10px] right-[10px] text-[16px] cursor-pointer' ></i></span>
+    </div>
+      `;
+  }
+  $(".toastBox").append(toast);
+
+  setTimeout(() => {
+    toast.remove();
+  }, 3000);
 }
 //#endregion
