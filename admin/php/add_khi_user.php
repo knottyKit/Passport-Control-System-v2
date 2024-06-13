93199aed51b0a55d928550ed3678ba09999dd08d
Dexmel <hernandez-kdt@global.kasaki.com>
2024-06-13 12:34:06 +0800

    update bug on adding existing user

--- admin/php/add_khi_user.php
+++ admin/php/add_khi_user.php
@@ -36,10 +36,10 @@ $conn_pcs_disable->beginTransaction();
 
 #region main query
 try {
-    $checkID = "SELECT COUNT(*) FROM `khi_details` WHERE `number` = :empID AND `is_active` = 1";
+    $checkID = "SELECT `is_active` FROM `khi_details` WHERE `number` = :empID AND `is_active` = 1";
     $checkIDStmt = $conn_pcs_disable->prepare($checkID);
     $checkIDStmt->execute([":empID" => "$empID"]);
-    $checkCount = $checkIDStmt->fetchColumn();
+    $checkCount = $checkIDStmt->rowCount();
 
     if ($checkCount == 0) {
         $insertUser = "INSERT INTO `khi_details`(`number`, `surname`, `firstname`, `group_id`, `is_active`) 
@@ -63,6 +63,28 @@ try {
             }
         }
     } else {
+        $isActive = $checkIDStmt->fetchColumn();
+        if($isActive == 0) {
+            $updateUser = "UPDATE `khi_details` SET `surname` = :lname, `firstname` = :fname, `group_id` = :grpID, `is_active` = 1 WHERE `number` = :empID";
+            $updateUserStmt = $conn_pcs_disable->prepare($updateUser);
+            if($updateUserStmt->execute([":empID" => "$empID", ":lname" => "$lname", ":fname" => "$fname", ":grpID" => "$grpID"])) {
+                if ($empacc == 1) {
+                    $insertAccess = "INSERT INTO `khi_user_permissions`(`permission_id`, `employee_id`) VALUES (1, :empID)";
+                    $insertAccessStmt = $conn_pcs_disable->prepare($insertAccess);
+                    if ($insertAccessStmt->execute([":empID" => "$empID"])) {
+                        $conn_pcs_disable->commit();
+                        $message["isSuccess"] = 1;
+                        $message["message"] = "User successfully added";
+                    } else {
+                        $conn_pcs_disable->rollBack();
+                    }
+                } else {
+                    $conn_pcs_disable->commit();
+                    $message["isSuccess"] = 1;
+                    $message["message"] = "User successfully added";
+                }
+            }
+        }
         $conn_pcs_disable->rollBack();
         $message["isSuccess"] = 0;
         $message["message"] = "User ID already registered";
